56633e89d024573e1a3f9e0e7f8ef86a94a9df16
Pradityo Adi WICAKSONO <pradityo.wicaksono@yahoo.com>
2025-08-12 00:28:07 +0200

    prepopulate database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO STOCKS (uid, name, price, stock_inventaire) VALUES ('a1b2c3d4-e5f6-4a7b-8c9d-0e1f2a3b4c5d', 'Vélo gravel', 1200.00, 200);
+INSERT INTO STOCKS (uid, name, price, stock_inventaire) VALUES ('b2c3d4e5-f6a7-4b8c-9d0e-1f2a3b4c5d6e', 'iPhone 15', 800.00, 150);
+INSERT INTO STOCKS (uid, name, price, stock_inventaire) VALUES ('c3d4e5f6-a7b8-4c9d-0e1f-2a3b4c5d6e7f', 'Tablet', 400.00, 1);
